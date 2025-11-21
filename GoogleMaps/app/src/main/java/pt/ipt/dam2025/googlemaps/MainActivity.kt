7250d3bb9ea5590927d760ccdc9bdd5143ffe971
jcnpereira <casimiro@ipt.pt>
2025-11-21 21:17:58 +0000

    Mapa do Google Maps a aparecer na app

--- GoogleMaps/app/src/main/java/pt/ipt/dam2025/googlemaps/MainActivity.kt
+++ GoogleMaps/app/src/main/java/pt/ipt/dam2025/googlemaps/MainActivity.kt
@@ -5,16 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import pt.ipt.dam2025.googlemaps.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    /* ##############################################################
+           DO NOT FORGET THAT YOU MUST ASK FOR PERMISSION TO USE GPS DATA
+           ############################################################## */
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker at IPT, Tomar.
+     * If Google Play services is not installed on the device,
+     * the user will be prompted to install it inside the SupportMapFragment.
+     * This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Tomar and move the camera to it
+        val tomar = LatLng(39.59955518526609, -8.389549138467965)
+        mMap.addMarker(MarkerOptions().position(tomar).title("Marker in IPT (I154)"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tomar))
+        //                                          .newLatLngZoom(tomar,15f))
+    }
+
+
+
+
 }
